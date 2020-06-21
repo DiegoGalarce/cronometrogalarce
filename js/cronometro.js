2785d4132f0d713c4d91bda6a466f7590cb4fd57
Diego Galarce <galarcediego@gmail.com>
2020-06-21 20:12:02 -0300

    nada

--- js/cronometro.js
+++ js/cronometro.js
@@ -1,22 +1,60 @@
-let contadorS = 0;
-let contadorM = 0;
-function crono(){
-    let pHora = document.getElementById("horas");
-    let pMinutos = document.getElementById("minutos");
-    let pSegundos = document.getElementById("segundos");
+let secons = 0;
+let minut = 0;
+let hora = 0;
 
-window.setInterval(
-    function(){
-        if(contadorS==60){
-            contadorS = 0;
-            contadorM++;
-            pMinutos.innerHTML = "0" + contadorM;
-            if(contadorM==0){
-               contadorM=0;
-            }
-            pSegundos.innerHTML = "0" + contadorS;
-         }
+function Empezar(){
+    control = setInterval(cronometro, 10);
+    document.getElementById("Empezar").disabled = true;
+    document.getElementById("Reiniciar").disabled = false;
+    document.getElementById("Parar").disabled = false;
+} console.log("Parar")
+
+function Parar(){
+    clearInterval(control);
+    document.getElementById("Parar").disabled = true;
+}
+
+function Reiniciar(){
+clearInterval(control);
+secons = 0;
+minut = 0; 
+hora = 0;
+Segundos.innerHTML = "00";
+Minutos.innerHTML = "00";
+Hora.innerHTML = "00";
+document.getElementById("Empezar").disabled = false;
+document.getElementById("Reiniciar").disabled = true;
+document.getElementById("Parar").disabled = true;
+}
+
+
+function cronometro(){
+   if (minut == 0) {
+        secons++;
+        if (secons < 10) { secons = "0"+ secons }
+        Segundos.innerHTML = secons;
+       }
+    if(secons == 59){
+        secons = -1;
     }
-,1000);
+    if(secons == 0){
+        minut++;
+        if(minut < 10){
+            minut = "0" + minut;
+        }
+        Minutos.innerHTML = minut;
+    }
+    if(minut == 59){
+        minut = -1
+    }
+    if(secons == 0 && minut == 0){
+       hora++;
+       if(hora < 10){
+       hora = "0" + hora
+       }
+       Horas.innerHTML = hora
+    }
+    
+    
 
-} 
+}
