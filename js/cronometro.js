bf0c65527c862ba8c6e768d79b4f36c31d14d7cf
Diego Galarce <galarcediego@gmail.com>
2020-06-23 11:44:59 -0300

    se completo el ejercicio de cronometro con function y query

--- js/cronometro.js
+++ js/cronometro.js
@@ -1,60 +1,54 @@
+let milesima = 0;
 let secons = 0;
 let minut = 0;
-let hora = 0;
 
-function Empezar(){
+function Empezar() {
     control = setInterval(cronometro, 10);
     document.getElementById("Empezar").disabled = true;
     document.getElementById("Reiniciar").disabled = false;
     document.getElementById("Parar").disabled = false;
-} console.log("Parar")
-
-function Parar(){
+}
+function Parar() {
     clearInterval(control);
     document.getElementById("Parar").disabled = true;
 }
-
-function Reiniciar(){
-clearInterval(control);
-secons = 0;
-minut = 0; 
-hora = 0;
-Segundos.innerHTML = "00";
-Minutos.innerHTML = "00";
-Hora.innerHTML = "00";
-document.getElementById("Empezar").disabled = false;
-document.getElementById("Reiniciar").disabled = true;
-document.getElementById("Parar").disabled = true;
+function Reiniciar() {
+    clearInterval(control);
+    milesima = 0;
+    secons = 0;
+    minut = 0;
+    MileSima.innerHTML = "00";
+    Segundos.innerHTML = "00";
+    Minutos.innerHTML = "00";
+    document.getElementById("Empezar").disabled = false;
+    document.getElementById("Reiniciar").disabled = true;
+    document.getElementById("Parar").disabled = true;
 }
+function cronometro() {
+    milesima = milesima + 1;
+    //MileSima.innerHTML = milesima;
+    if (milesima < 10) {
+        MileSima.innerHTML = "0" + milesima;
+    } else {
+        MileSima.innerHTML = milesima;
+    }
 
-
-function cronometro(){
-   if (minut == 0) {
+    if (milesima == 59) {
+        milesima = 0;
         secons++;
-        if (secons < 10) { secons = "0"+ secons }
-        Segundos.innerHTML = secons;
-       }
-    if(secons == 59){
-        secons = -1;
-    }
-    if(secons == 0){
-        minut++;
-        if(minut < 10){
-            minut = "0" + minut;
+        if (secons < 10) {
+            Segundos.innerHTML = "0" + secons;
+        } else {
+            Segundos.innerHTML = secons;
         }
-        Minutos.innerHTML = minut;
+        if (secons == 59) {
+            secons = 0;
+            minut++;
+            if (minut < 10) {
+                Minutos.innerHTML = "0" + minut;
+            } else {
+                Minutos.innerHTML = minut;
+            }
+        }
     }
-    if(minut == 59){
-        minut = -1
-    }
-    if(secons == 0 && minut == 0){
-       hora++;
-       if(hora < 10){
-       hora = "0" + hora
-       }
-       Horas.innerHTML = hora
-    }
-    
-    
-
 }
